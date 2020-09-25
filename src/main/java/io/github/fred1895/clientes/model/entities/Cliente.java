127e5c0a471a8c4a8516dccd107fe41ca3cc3c46
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-25 12:08:28 -0300

    Testando conexao com banco de dados

--- src/main/java/io/github/fred1895/clientes/model/entities/Cliente.java
+++ src/main/java/io/github/fred1895/clientes/model/entities/Cliente.java
@@ -1,12 +1,18 @@
 package io.github.fred1895.clientes.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +24,11 @@ public class Cliente {
     @Column(nullable = false, length = 11)
     private String cpf;
 
-    @Column()
+    @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDate.now());
+    }
 }
