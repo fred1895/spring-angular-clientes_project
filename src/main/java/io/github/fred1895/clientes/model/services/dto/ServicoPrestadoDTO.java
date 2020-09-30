a8e9acc6edded1383bb5b743d77d6ff0f113974e
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-30 18:15:45 -0300

    Adicionando validação para serviços prestados

--- src/main/java/io/github/fred1895/clientes/model/services/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/fred1895/clientes/model/services/dto/ServicoPrestadoDTO.java
@@ -5,15 +5,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
 
+    @NotEmpty(message = "O campo descrição é obrigatório")
     private String descricao;
+
+    @NotEmpty(message = "O campo preço é obrigatório")
     private String preco;
+
+    @NotEmpty(message = "O campo data é obrigatório")
     private String data;
+
+    @NotNull(message = "O campo cliente é obrigatório")
     private Long idCliente;
 
 }
--- src/main/java/io/github/fred1895/clientes/rest/ServicoPrestadoResource.java
+++ src/main/java/io/github/fred1895/clientes/rest/ServicoPrestadoResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,7 @@ public class ServicoPrestadoResource {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO obj) {
+    public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO obj) {
         return service.salvar(obj);
     }
 
