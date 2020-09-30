22811fcde4ca3145af7a253a6b4e53ff5cb38ae2
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-30 16:09:44 -0300

    Adicionando @CrossOrigin para ServicoPrestadoResource

--- src/main/java/io/github/fred1895/clientes/rest/ServicoPrestadoResource.java
+++ src/main/java/io/github/fred1895/clientes/rest/ServicoPrestadoResource.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/servicos-prestados")
+@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoResource {
 
     @Autowired
